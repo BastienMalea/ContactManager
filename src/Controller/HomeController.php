2f27eaeaf3da8dbcbb61ffab8d0260dd500c96ab
BastienMalea <bastmalea@gmail.com>
2023-12-24 18:58:57 +0100

    affichage contacts homeview

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home.index', methods: ['GET'])]
-    public function index(): Response
+    public function index(ContactRepository $repository): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $contacts = $repository->findAll();
+
+        return $this->render('pages/index.html.twig', [
+            'contacts' => $contacts,
         ]);
     }
 }
