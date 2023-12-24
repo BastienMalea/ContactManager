ae825dc870d44d6be06d328cf1a9a43d8305e525
BastienMalea <bastmalea@gmail.com>
2023-12-24 17:48:36 +0100

    Ajout fixture contact

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Contact;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 0; $i < 50; $i++){
+            $contact = new Contact();
+            $contact ->setName($this->faker->word())
+                ->setFirstname($this->faker->word())
+                ->setPhoneNumber($this->faker->regexify('[0-9]{10}'))
+                ->setEmail($this->faker->email());
+            $manager->persist($contact);
+        }
 
         $manager->flush();
     }
