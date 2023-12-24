ca533b483986929e6966cb80f5a026fbd930b618
BastienMalea <bastmalea@gmail.com>
2023-12-24 19:08:04 +0100

    Ajout pagination contact table

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\ContactRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home.index', methods: ['GET'])]
-    public function index(ContactRepository $repository): Response
+    public function index(ContactRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        $contacts = $repository->findAll();
+        $contacts = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
 
         return $this->render('pages/index.html.twig', [
             'contacts' => $contacts,
