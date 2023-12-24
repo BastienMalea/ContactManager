04af9446a0548fd361a7e6e9e8105ae2303509b2
BastienMalea <bastmalea@gmail.com>
2023-12-24 19:31:23 +0100

    Suppression HomeController

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/', name: 'contact.index' , methods: ['GET'])]
+    public function index(ContactRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
+        $contacts = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
         return $this->render('pages/contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'contacts' => $contacts,
         ]);
+    }
+
+    #[Route('/new', name: 'contact.new', methods: ['GET', 'POST'])]
+    public function new() : Response
+    {
+        return $this->render('pages/contact/new.html.twig');
     }
 }
