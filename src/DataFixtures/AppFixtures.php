ab8be37bf387f6a53420404d26dbd5be63e4e1d3
BastienMalea <bastmalea@gmail.com>
2023-12-27 18:57:49 +0100

    Ajout fixture group

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Contact;
+use App\Entity\Group;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -18,13 +19,32 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
-        for($i = 0; $i < 50; $i++){
+        // Création des contacts
+        $contacts = [];
+        for ($i = 0; $i < 50; $i++) {
             $contact = new Contact();
-            $contact ->setName($this->faker->word())
-                ->setFirstname($this->faker->word())
+            $contact->setName($this->faker->lastName())
+                ->setFirstname($this->faker->firstName())
                 ->setPhoneNumber($this->faker->regexify('[0-9]{10}'))
                 ->setEmail($this->faker->email());
             $manager->persist($contact);
+            $contacts[] = $contact;
+        }
+
+        // Création des groupes et assignement des contacts aléatoirement
+        for ($j = 0; $j < 10; $j++) {
+            $group = new Group();
+            $group->setName($this->faker->word());
+
+            // Déterminer un nombre aléatoire de contacts à ajouter au groupe
+            $numContacts = mt_rand(1, count($contacts));
+            for ($k = 0; $k < $numContacts; $k++) {
+                // Sélectionner un contact aléatoire et l'ajouter au groupe
+                $randomContact = $contacts[mt_rand(0, count($contacts) - 1)];
+                $group->addMember($randomContact);
+            }
+
+            $manager->persist($group);
         }
 
         $manager->flush();
